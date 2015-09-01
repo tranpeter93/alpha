48eef1af27469907faa6860d455cd82647b77626
Peter Tran <tranpeter93@gmail.com>
2015-08-31 20:16:38 -0700

    Implement Login Flow

--- js/fb.js
+++ js/fb.js
@@ -6,16 +6,31 @@ window.fbAsyncInit = function() {
 	  version    : 'v2.4'
 	});		
 	
-	FB.login(function(response) {
-		if (response.authResponse) {
-			console.log('Welcome!  Fetching your information.... ');
-			FB.api('/me', function(response) {
-		    console.log('Good to see you, ' + response.name + '.');
-		 });
-		} else {
-			console.log('User cancelled login or did not fully authorize.');
+	FB.getLoginStatus(function(response) {
+		if (response.status === 'connected') {		
+			var uid = response.authResponse.userID;
+			var accessToken = response.authResponse.accessToken;
+			console.log("Admin: You're already logged in!");
+		} 
+		else if (response.status === 'not_authorized') {
+		// the user is logged in to Facebook, but has not authenticated your app
+			alert("Admin: You did not authenticate. Please refresh to authenticate. Thank you.");
+		} 
+		else {
+		// the user isn't logged in to Facebook.
+			FB.login(function(prompt) {
+				if (prompt.authResponse) {
+					console.log('Welcome!  Fetching your information.... ');
+					FB.api('/me', function(prompt) {
+					console.log('Good to see you, ' + prompt.name + '.');
+				 });
+				} else {
+					console.log('User cancelled login or did not fully authorize.');
+				}
+			});
 		}
-	});
+	 });
+	
 };
 
 $(document).ready(function() {
@@ -27,14 +42,5 @@ $(document).ready(function() {
 		});     
 	});
 	
-	/*
-	(function(d, s, id) {
-		 var js, fjs = d.getElementsByTagName(s)[0];
-		 if (d.getElementById(id)) {return;}
-		 js = d.createElement(s); 
-		 js.id = id;
-		 js.src = "js/fb-sdk.js";	 
-		 fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-	*/
+	
 });
