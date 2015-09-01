566321754d2a46a92b26f3ec834411ee80cf7040
Peter Tran <tranpeter93@gmail.com>
2015-08-31 21:00:49 -0700

    Temporary
    - Removing redirection for manual FB SDK testing
    - Container (bootstrap css)
    - rename tab title

--- js/fb.js
+++ js/fb.js
@@ -13,7 +13,8 @@ window.fbAsyncInit = function() {
 			var uid = response.authResponse.userID;
 			var accessToken = response.authResponse.accessToken;
 			console.log("Admin: You're already logged in!");
-			window.location.href = "home.html"
+			//window.location.href = "home.html";
+			
 		} 
 		else if (response.status === 'not_authorized') {
 			// the user is logged in to Facebook, but has not authenticated your app
