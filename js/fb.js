2e03df2fe083f559a9eb2af86593a79a6ae529d7
Peter Tran <tranpeter93@gmail.com>
2015-08-31 20:03:51 -0700

    Jquery implementation of getting FB JS SDK #1

--- js/fb.js
+++ js/fb.js
@@ -5,9 +5,31 @@ window.fbAsyncInit = function() {
 	  xfbml      : true,
 	  version    : 'v2.4'
 	});		
+	
+	FB.login(function(response) {
+		if (response.authResponse) {
+			console.log('Welcome!  Fetching your information.... ');
+			FB.api('/me', function(response) {
+		    console.log('Good to see you, ' + response.name + '.');
+		 });
+		} else {
+			console.log('User cancelled login or did not fully authorize.');
+		}
+	});
 };
 
 $(document).ready(function() {
+	$.ajaxSetup({ cache: true });
+	$.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+    FB.init({
+      appId: '{1057857254231943}',
+      version: 'v2.4' 
+    });     
+    $('#loginbutton,#feedbutton').removeAttr('disabled');
+    FB.getLoginStatus(updateStatusCallback);
+
+	
+	/*
 	(function(d, s, id) {
 		 var js, fjs = d.getElementsByTagName(s)[0];
 		 if (d.getElementById(id)) {return;}
@@ -16,4 +38,5 @@ $(document).ready(function() {
 		 js.src = "js/fb-sdk.js";	 
 		 fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
+	*/
 });
