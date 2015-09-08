d7502da4ea5fadac0300e93de4ee6e9660216847
Peter Tran <tranpeter93@gmail.com>
2015-09-07 19:14:10 -0700

    gapi Signin

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-	var clientId = "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
-	
+
 	$("#login").click(function() {
 		gapi.load('auth2', function() {
-			client_id: clientId;
+			client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
 		});
 
-		gapi.auth2.init({client_id: clientId});		
+		var auth = gapi.auth2.init({client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com",
+		fetch_basic_profile: true});		
+		
+		auth.signIn();
 	});
 });
