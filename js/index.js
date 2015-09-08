968d7d983093ee87ef16548e9fe7bb82c649b515
Peter Tran <tranpeter93@gmail.com>
2015-09-07 18:43:56 -0700

    gapi init

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+	var clientId = "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
+	
 	$("#login").click(function() {
-		gapi.auth2.init({
-			client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com"
-		})		
+		gapi.load('auth2', function() {
+			client_id: clientId;
+		});
+
+		gapi.auth2.init({client_id: clientId});		
 	});
 });
