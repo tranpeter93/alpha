15ac73fb136b0b7c888cc61c22cc49e921b83332
Peter Tran <tranpeter93@gmail.com>
2015-09-07 20:01:45 -0700

    test .then

--- js/index.js
+++ js/index.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
 	$("#login").click(function() {
 		gapi.load('auth2', function() {
 			client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
-		}).then(gapi.auth2.init(
+		})	
+		
+		
+	}).then(gapi.auth2.init(
 			{
 				client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com"
-			}));		
-		
-		auth.signIn();
-	});
+			})
+	).then(auth.signIn(););
 });
