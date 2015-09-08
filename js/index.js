a89fcd0b7c93809acd1a2f47f4998711771ac7ea
Peter Tran <tranpeter93@gmail.com>
2015-09-07 20:16:15 -0700

    Cleanse oauth

--- js/index.js
+++ js/index.js
@@ -2,13 +2,17 @@ $(document).ready(function() {
 
 	$("#login").click(function() {
 		gapi.load('auth2', function() {
-			client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
-		})	
-		
-		
-	}).then(gapi.auth2.init(
-			{
-				client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com"
-			})
-	).then(auth.signIn());
+			var auth2 = gapi.auth2.init({client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com"});
+			
+			auth2.attachClickHandler("login", {}, onSuccess, onFailure);
+		});
+	});
 });
+
+var onSuccess = function(user) {
+	console.log('Signed in as ' + user.getBasicProfile().getName())
+}
+
+var onFailure = function(error) {
+    console.log(error);
+};
