5d3e2e4111e11918a09e7fa52f1b0c252782d3c0
Peter Tran <tranpeter93@gmail.com>
2015-09-07 19:59:29 -0700

    test

--- js/index.js
+++ js/index.js
@@ -3,12 +3,10 @@ $(document).ready(function() {
 	$("#login").click(function() {
 		gapi.load('auth2', function() {
 			client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
-		});
-
-		var auth = gapi.auth2.init(
+		}).then(gapi.auth2.init(
 			{
 				client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com"
-			});		
+			}));		
 		
 		auth.signIn();
 	});
