4a544a21dcfe428b0da241b9130df9f57bdd09a5
Peter Tran <tranpeter93@gmail.com>
2015-09-08 18:22:57 -0700

    Testing Hangouts

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
+var gapi_clientId = "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
 
+$(document).ready(function() {
 	$("#login").click(function() {
 		gapi.load('auth2', function() {
-			var auth2 = gapi.auth2.init({client_id: "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com"});
+			var auth2 = gapi.auth2.init({client_id: gapi_clientId});
 			
 			auth2.attachClickHandler("login", {}, onSuccess, onFailure);
 		});
@@ -11,6 +12,7 @@ $(document).ready(function() {
 
 var onSuccess = function(user) {
 	console.log('Signed in as ' + user.getBasicProfile().getName())
+	//window.location.href = "http://ec2-52-88-95-120.us-west-2.compute.amazonaws.com/staging/alpha.html"
 }
 
 var onFailure = function(error) {
