c1e76abbdf877f1ec47b7e8c328ab0827c3ff858
Peter Tran <tranpeter93@gmail.com>
2015-09-16 19:40:24 -0700

    Removed auth2/Replaced with auth

--- js/index.js
+++ js/index.js
@@ -1,18 +1,24 @@
 var gapi_clientId = "43916539739-kg913v794v0egih5l4en19eermk35qgl.apps.googleusercontent.com";
 
-$(document).ready(function() {
-	gapi.load('auth2', function() {
-		var auth2 = gapi.auth2.init({client_id: gapi_clientId});
-		
-		gapi.hangout.render('login', 
-		{
-			'render': 'createhangout',
-			'initial_apps': 
-			{
-				'app_id': '43916539739'
-			},
-			'widget_size': 250
-		})
-	});
+var SCOPE = ["https://www.googleapis.com/auth/plus.login",
+					"https://mail.google.com/", 
+					"https://www.googleapis.com/auth/gmail.modify", 
+					"https://www.googleapis.com/auth/gmail.readonly"];
 
+$(document).ready(function() {
+	gapi.load('auth', function() {
+		gapi.auth.authorize(
+		{
+				'client_id': gapi_clientId,
+				'scope': SCOPE,
+				'immediate': false
+		});
+		
+		gapi.load('client', function() {
+			gapi.client.load('gmail', 'v1').then(function() {
+               var resp = gapi.client.request({"path": "https://www.googleapis.com/gmail/v1/users/me/threads"});
+               resp.execute(function(a) {console.log(a)});
+            });	
+		});
+	});
 });
